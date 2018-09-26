37196700545e37a3a953f53eb69b43fd581ba03d
caleb <calebpomazon@gmail.com>
2018-09-26 15:30:33 -0400

    Changed the entire layout of the site. Added new arrows.

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 function myFunction(x) {
 	x.classList.toggle("change");
 }
+
+$('.roundRight').click(function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    $('.arrowRight').toggleClass('moveRight');
+});
+
+$('.roundLeft').click(function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    $('.arrowLeft').toggleClass('moveLeft');
+});
+
+$('.roundUp').click(function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    $('.arrowUp').toggleClass('moveUp');
+});
+
+$('.roundDown').click(function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    $('.arrowDown').toggleClass('moveDown');
+});
